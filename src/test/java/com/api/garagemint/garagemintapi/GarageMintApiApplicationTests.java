766865f7794ab987e2539767a88094d1d2b2b94d
Fatih Şengül <111802646+Fatih-Sengul@users.noreply.github.com>
2025-09-05 14:13:43 +0300

    Switch to PostgreSQL with profile-specific configs

--- src/test/java/com/api/garagemint/garagemintapi/GarageMintApiApplicationTests.java
+++ src/test/java/com/api/garagemint/garagemintapi/GarageMintApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.api.garagemint.garagemintapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GarageMintApiApplicationTests {
 
     @Test
